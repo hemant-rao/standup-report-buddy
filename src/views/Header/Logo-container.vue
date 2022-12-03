0b320df7268e29eb4aac48866739f4b230baa19e
hemant-rao <mrhrao@gmail.com>
2022-12-03 18:33:29 +0530

    logo container, name change

---
+++ src/views/Header/Logo-container.vue
@@ -0,0 +1,14 @@
+<template>
+  <Logo />
+</template>
+<script>
+import Logo from "../../components/common/logo.vue";
+export default {
+  name: "logo",
+  components: {
+    Logo,
+  },
+  setup() {},
+};
+</script>
+<style lang="scss" scoped></style>
