995ff7857f4bde443a4cce9a80b49e365ab121f6
hemant-rao <mrhrao@gmail.com>
2022-12-04 22:14:50 +0530

    child routing for reporting component and add report nav button

--- src/router/index.js
+++ src/router/index.js
@@ -1,67 +1,83 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login/Login.vue'
-import Registration from '../views/Registration/Registration.vue'
-import Dashboard from '../views/Dashboard/Dashboard.vue'
-import Forgot from "../views/Forgot/Forgot.vue"
-import ChangePassword from "../views/Change_Password/ChangePassword.vue"
-import OldReport from "../views/Report/OldReport.vue"
-import TodayReport from "../views/Report/TodayReport.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login/Login.vue";
+import Registration from "../views/Registration/Registration.vue";
+import Dashboard from "../views/Dashboard/Dashboard.vue";
+import Forgot from "../views/Forgot/Forgot.vue";
+import ChangePassword from "../views/Change_Password/ChangePassword.vue";
+import Reports from "../views/Report/Reports.vue";
+import OldReport from "../views/Report/OldReport.vue";
+import TodayReport from "../views/Report/TodayReport.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/registration',
-    name: 'Registration',
-    component: Registration
+    path: "/registration",
+    name: "Registration",
+    component: Registration,
   },
   {
-    path: '/forgot',
-    name: 'Forgot',
-    component: Forgot
+    path: "/forgot",
+    name: "Forgot",
+    component: Forgot,
   },
   {
-    path: '/change-password',
-    name: 'ChangePassword',
-    component: ChangePassword
+    path: "/change-password",
+    name: "ChangePassword",
+    component: ChangePassword,
   },
   {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard
+    path: "/dashboard",
+    name: "Dashboard",
+    component: Dashboard,
   },
   {
-    path: '/old-report',
-    name: 'OldReport',
-    component: OldReport
+    path: "/reports",
+    component: Reports,
+    children: [
+      {
+        path: "",
+        name: "TodayReport",
+        component: TodayReport,
+        // redirect: 'today-report',
+      },
+      {
+        path: "today-report",
+        name: "TodayReport",
+        component: TodayReport,
+      },
+      {
+        path: "old-report",
+        name: "OldReport",
+        component: OldReport,
+      },
+    ],
   },
+
   {
-    path: '/today-report',
-    name: 'TodayReport',
-    component: TodayReport
-  },
-  {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+];
 
 const router = createRouter({
+  mode: 'history',
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
--- src/views/Header/NevBar.vue
+++ src/views/Header/NevBar.vue
@@ -17,13 +17,43 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="navbar-nav mr-auto">
-          <router-link to="/" class="nav-item nav-link">Home</router-link>
-          <router-link to="/about" class="nav-item nav-link">About</router-link>
-          <router-link to="/login" class="nav-link">Login</router-link>
-          <router-link to="/registration" class="nav-link"
+          <router-link to="/" active-class="active" class="nav-item nav-link"
+            >Home</router-link
+          >
+          <router-link
+            to="/about"
+            active-class="active"
+            class="nav-item nav-link"
+            >About</router-link
+          >
+          <router-link
+            tag="li"
+            active-class="active"
+            to="/login"
+            class="nav-link"
+            >Login</router-link
+          >
+          <router-link
+            tag="li"
+            active-class="active"
+            to="/registration"
+            class="nav-link"
             >Registration</router-link
           >
-          <router-link to="/dashboard" class="nav-link">Dashboard</router-link>
+          <router-link
+            tag="li"
+            active-class="active"
+            to="/dashboard"
+            class="nav-link"
+            >Dashboard</router-link
+          >
+          <router-link
+            tag="li"
+            active-class="active"
+            to="/reports"
+            class="nav-link"
+            >Reports</router-link
+          >
         </div>
         <!-- <form class="d-flex">
           <input
@@ -48,9 +78,9 @@ export default {
   setup() {},
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 #nav {
-  padding: 30px;
+  // padding: 30px;
   text-align: center;
 }
 
@@ -59,10 +89,22 @@ export default {
   color: #2c3e50;
 }
 
-#nav a.router-link-exact-active {
+#nav a.router-link-exact-active,
+.active.nav-link {
   color: black;
   background: transparent;
-  border-bottom: 1px solid #4285f4;
+  border-bottom: 1px solid #4285f4 !important;
+}
+.nav {
+  text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: nowrap;
+  align-items: center;
+  .nav-link {
+    border-bottom: 1px solid #fff;
+  }
 }
 @media (min-width: 992px) {
   .navbar-expand-lg .navbar-collapse {
